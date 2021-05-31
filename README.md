b046880cb26eee6529606c0f8264523fd7f2b719
Joaquín Soliño <joaquin.solino@me.com>
2021-05-30 22:37:52 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # Coursera_RegrMod_CourseProject
-Course Project for Regression Models - JSP
+Course Project for Regression Models - Joaquín Soliño
+
+### Prompts for the project 
+
+You work for Motor Trend, a magazine about the automobile industry. Looking at a data set of a collection of cars, they are interested in exploring the relationship between a set of variables and miles per gallon (MPG) (outcome). They are particularly interested in the following two questions:
+
+“Is an automatic or manual transmission better for MPG”
+"Quantify the MPG difference between automatic and manual transmissions"  
+Review criteria
+
+
+### Peer Grading
+
+The criteria that will use to evaluate and grade the  work are shown below.
+
+> Each criteria is binary: (1 point = criteria met acceptably; 0 points = criteria not met acceptably)
+
+Criteria
+
+1. Interpret the coefficients correctly
+2. Do some exploratory data analyses
+3. Fit multiple models and detail their strategy for model selection
+4. Answer the questions of interest or detail why the question(s) is (are) not answerable
+5. Do a residual plot and some diagnostics
+6. Quantify the uncertainty in their conclusions and/or perform an inference correctly
+7. The report must be brief (about 2 pages long) for the main body of the report and no longer than 5 with supporting appendix of figures
+8. The report must include an executive summary
+9. The report must be done in Rmd (knitr)
+
+### Question
+
+Take the \color{red}{\verb|mtcars|}mtcars data set and write up an analysis to answer their question using regression models and exploratory data analyses.
+
+Your report must be:
+
++ Written as a PDF printout of a compiled (using knitr) R markdown document.
++ Brief. Roughly the equivalent of 2 pages or less for the main text. Supporting figures in an appendix can be included up to 5 total pages including the 2 for the main report. + The appendix can only include figures.
++ Include a first paragraph executive summary.
++ Upload your PDF by clicking the Upload button below the text box.
+
+### Peer Grading
+
+The criteria that your classmates will use to evaluate and grade your work are shown below. 
+
++ Each criteria is binary: (1 point = criteria met acceptably; 0 points = criteria not met acceptably)
++ Your Course Project score will be the sum of the points and will count as 40% of your final grade in the course. 
+
---
+++ Script.R
@@ -0,0 +1,92 @@
+library(datasets)
+library(dplyr)
+library(ggplot2)
+library(GGally)
+
+
+head(mtcars)
+?mtcars
+
+##########################################
+# MPG vs transmision type - Boxplot
+##########################################
+my_cars <- mtcars %>% 
+        mutate( mpg = mpg,
+                   am = factor(am, labels = c("manual","automatic")),
+                   vs = factor(vs, labels = c("V","S")))
+
+my_cars %>% ggplot(aes(x = am, y = mpg)) +
+        geom_boxplot() +
+        geom_jitter(aes(color = am), size = 4, alpha = .35) +
+        labs(title = "MPG vs transmision type",
+             x = "transmision type",
+             y = "mpg")+
+        theme(axis.text.x = element_text(
+                angle = 0,
+                size = 8,
+                vjust = 0.5
+        ),
+        panel.background = element_rect(fill = "grey90"),
+        panel.grid = element_line(color = "grey"),
+        legend.position = "bottom"
+        ) 
+
+my_cars %>% ggplot(aes(x = vs, y = mpg)) +
+        geom_boxplot() +
+        geom_jitter(aes(color = vs), size = 4, alpha = .35) +
+        labs(title = "MPG vs motor shape",
+             x = "v-shape / straight",
+             y = "mpg")+
+        theme(axis.text.x = element_text(
+                angle = 0,
+                size = 8,
+                vjust = 0.5
+        ),
+        panel.background = element_rect(fill = "grey90"),
+        panel.grid = element_line(color = "grey"),
+        legend.position = "bottom"
+        ) 
+
+
+
+##########################################
+# MPG vs transmission type - Boxplot
+##########################################
+cor(my_cars[c(1,2,3,4,5,6,7,10,11)], my_cars[c(1,2,3,4,5,6,7,10,11)])
+
+# test hypothesis
+with(my_cars,t.test(mpg ~ am))
+with(my_cars,t.test(mpg ~ vs))
+
+
+fit_trans <- lm(mpg ~ am, data = my_cars)
+summary(fit_trans)
+fit_trans$coefficients
+mean(fit_trans$residuals)
+
+fit_all <- lm(mpg ~ ., data = my_cars)
+summary(fit_all)
+fit_all$coefficients
+mean(fit_all$residuals)
+
+fit_5 <- lm(mpg ~ am + wt + qsec + drat + gear , data = my_cars)
+summary(fit_5)
+fit_5$coefficients
+mean(fit_5$residuals)
+
+fit_3 <- lm(mpg ~ am + wt + qsec, data = my_cars)
+summary(fit_3)
+fit_3$coefficients
+mean(fit_3$residuals)
+
+
+par(mfrow = c(2,2))
+plot(fit_trans)
+plot(fit_all)
+plot(fit_4)
+
+summary(my_cars)
+ggpairs(my_cars, lower = list(continuous = "smooth"),title = "mtcars - Pairs")
+
+
+
